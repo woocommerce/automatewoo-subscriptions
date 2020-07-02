affdd218713aa42347af7cae6f8ed77187869f0d
Dan Bitzer <danielbitzer@gmail.com>
2020-07-02 16:26:03 +0930

    Update versions

--- automatewoo-subscriptions.php
+++ automatewoo-subscriptions.php
@@ -6,7 +6,7 @@
  * Author: WooCommerce
  * Author URI: https://woocommerce.com/
  * License: GPLv3
- * Version: 1.0.3
+ * Version: 1.1.0
  * Requires at least: 4.0
  * Tested up to: 5.4
  *
@@ -72,7 +72,7 @@ final class AutomateWoo_Subscriptions {
 	 *
 	 * @var string
 	 */
-	private $version = '1.0.4';
+	private $version = '1.1.0';
 
 	/**
 	 * Instance of singleton.
